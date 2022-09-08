d0a0757c849e8abc57f1b575fc0e806afe9c109b
Dayveed63 <davidferanmi148@gmail.com>
2022-09-08 07:47:04 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - print the string in the puts function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
