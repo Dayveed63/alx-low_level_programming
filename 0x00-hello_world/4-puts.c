870b94a3588373977c12c289c55672da8f33c3d9
Dayveed63 <davidferanmi148@gmail.com>
2022-09-08 09:12:29 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function 
+* main - print the string in the puts function
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
